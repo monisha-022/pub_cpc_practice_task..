4e469f960fbf8c516223e250aeb12e3bd134e9e8
Monisha farzana <monisha.pompy528@gmail.com>
2025-09-30 17:00:34 +0600

    README.c

--- README.c
+++ README.c
@@ -2,10 +2,6 @@ CPC C Practice
 
 This repo contains C programming problem solutions from CPC practice sessions.
 
-Solved Problems (BeeCrowd)  
 
 Language  
 C (GCC Compiler)
-
-Platform  
-BeeCrowd (Beginner Level)
